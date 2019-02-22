479e6f07dbba4d274c5abb7f60ba277866c290bc
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-02-22 14:00:01 -0500

    refactors robot_sort.py, passing small list time test

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -99,15 +99,15 @@ class SortingRobot:
         else:
             return False
 
-    def hold_item(self):
-        '''
-        Picks up the item at the robots current position
-        '''
-        self._item = self._list[self._position]
+    # def hold_item(self):
+    #     '''
+    #     Picks up the item at the robots current position
+    #     '''
+    #     self._item = self._list[self._position]
 
-    def drop_item(self):
-        self._list[self._position] = self._item
-        self._item = None
+    # def drop_item(self):
+    #     self._list[self._position] = self._item
+    #     self._item = None
 
     def swap_item(self):
         """
@@ -158,22 +158,41 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
+
+        # [3!, 2, 1] hold None SWAP
+        # [None!, 2, 1] hold 3, MOVE RIGHT
+        # [None, 3!, 1] hold 2, SWAP
+        # [2!, 3, 1] hold None, MOVE LEFT, SWAP
+        # [2, 3!, 1] hold None, MOVE RIGHT
+        # [2, None, 1!] hold 3, SWAP, MOVE RIGHT
+        # [2, None!, 3] hold 1 MOVE LEFT
+        # [2, 1, 3!] hold None, can't move right
+
+        # [2!, 2, 1] hold None SWAP, MOVE RIGHT
+        # [None, 2!, 1] hold 2, MOVE LEFT SWAP MOVE RIGHT
+        # [2, 2!, 1] hold None, SWAP MOVE RIGHT
+        #
+ 
         for i in range(len(self._list)):
             self.move_to_index(0)
             self.set_light_off()
-            self.hold_item()
             for j in range(i, len(self._list)):
+                self.swap_item()
                 if self.can_move_right():
                     self.move_right()
                     if self.compare_item() > 0:
                         self.swap_item()
                         self.move_left()
-                        self.drop_item()
+                        self.swap_item()
                         self.move_right()
-                        self.hold_item()
                         self.set_light_on()
                     else:
-                        self.hold_item()
+                        self.move_left()
+                        self.swap_item()
+                        self.move_right()
+                else:
+                    self.swap_item()                   
+                
             if not self.light_is_on():
                 return self._list
         return self._list
@@ -185,9 +204,9 @@ if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
-    # l = [5, 4, 3, 2, 1]
+    l = [5, 4, 3, 2, 1]
 
     robot = SortingRobot(l)
 
